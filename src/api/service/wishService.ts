e44edd79affa0573ab701d814ddef8e3669e798c
BKrasnov <bkrasnov235@gmail.com>
2023-03-07 16:20:01 +0700

    Create CRUD-functionality to wishService and update config AT-2

--- src/api/service/wishService.ts
+++ src/api/service/wishService.ts
@@ -6,7 +6,7 @@ import { http } from '..';
 import { CONFIG } from '../config';
 
 export namespace WishService {
-  const fileName = 'wish-item.json';
+  const fileName = 'item';
   const wishUrl = new URL(fileName, CONFIG.apiUrl);
 
   /**
@@ -14,8 +14,41 @@ export namespace WishService {
    */
   export async function fetchWishList(): Promise<Item[]> {
     const { data } = await http.get<ItemDto[]>(wishUrl.toString());
-
     const wishListItem = data.map(wishItem => ItemMapper.fromDto(wishItem));
+    return wishListItem;
+  }
+
+  /**
+   * Update item.
+   * @param item Item.
+   */
+  export async function addWishItem(item: Item): Promise<Item[]> {
+    await http.post(wishUrl.toString(), {
+      data: item,
+    });
+    const wishListItem = fetchWishList();
+    return wishListItem;
+  }
+
+  /**
+   * Update item.
+   * @param item Item.
+   */
+  export async function updateWishItem(item: Item): Promise<Item[]> {
+    await http.put(`${wishUrl.toString()}/${item.id}`, {
+      data: item,
+    });
+    const wishListItem = fetchWishList();
+    return wishListItem;
+  }
+
+  /**
+   * Delete item.
+   * @param id Item id.
+   */
+  export async function deleteWishItem(id: number): Promise<Item[]> {
+    await http.delete<ItemDto[]>(`${wishUrl.toString()}/${id}`);
+    const wishListItem = fetchWishList();
     return wishListItem;
   }
 }
