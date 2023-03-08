6c0c59a249d3eac1facaeec7d016412c59925871
BKrasnov <bkrasnov235@gmail.com>
2023-03-08 20:25:53 +0700

    Create a button to add fake data AT-3

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,36 @@
 /** @jsxImportSource @emotion/react */
 
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+
+import { useAppDispatch } from '../../store';
+import { addFakeWishItems } from '../../store/wish/dispatchers';
+
+import { Button } from '../Button';
 
 import { headerStyle } from './styles';
 
-export const Header: FC = () => (
-  <div css={headerStyle.header}>
-    <h1>Список желаний</h1>
-  </div>
-);
+export const Header: FC = () => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = useCallback(() => {
+    dispatch(addFakeWishItems());
+  }, [dispatch]);
+
+  return (
+    <div css={headerStyle.header}>
+      <h1>Список желаний</h1>
+      <div>
+        <Button
+          handleClick={handleClick}
+          style={headerStyle.button}
+          type="button"
+        >
+          Добавить фейковые данные
+        </Button>
+        <div>
+          <b>Много не нажимать)</b>
+        </div>
+      </div>
+    </div>
+  );
+};
