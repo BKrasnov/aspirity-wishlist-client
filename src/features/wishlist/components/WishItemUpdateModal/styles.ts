8e0161215518e42ac7141bd163063cb2e30a9376
BKrasnov <bkrasnov235@gmail.com>
2023-03-08 15:22:06 +0700

    Create WishItemUpdateModal AT-2

--- src/features/wishlist/components/WishItemUpdateModal/styles.ts
+++ src/features/wishlist/components/WishItemUpdateModal/styles.ts
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { generalColors } from '../../theme/variable';
+import { generalColors } from '../../../../theme/variable';
 
 export const modalStyle = {
   modal: css({
@@ -7,13 +7,15 @@ export const modalStyle = {
     left: '50%',
     top: '50%',
     transform: 'translate(-50%, -50%)',
-    minHeight: '70%',
+    minHeight: '80%',
+    minWidth: '800px',
     backgroundColor: 'white',
-    padding: '20px',
+    padding: '20px 70px',
     boxShadow: `0.5px 0.5px 2px ${generalColors.tertiary}`,
   }),
 
   h2: css({
+    textAlign: 'center',
     color: generalColors.secondary,
   }),
 };
