0cd09be39dd6ff55a62b5679f5a7b90726e6c0ad
BKrasnov <bkrasnov235@gmail.com>
2023-03-08 15:21:27 +0700

    Create WishItemForm AT-2

---
+++ src/features/wishlist/components/WishItemForm/WishItemForm.tsx
@@ -0,0 +1,96 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { FC } from 'react';
+import { Form, FormikProvider, useFormik } from 'formik';
+
+import {
+  initialFormValues,
+  ItemFormValue,
+  WishItemFormSchema,
+} from './formSettings';
+
+import {
+  FormikControl,
+  OptionsSelect,
+} from '../../../../components/FormikControl';
+import { Button } from '../../../../components/Button';
+
+import { buttonFormStyle } from './styles';
+
+const optionItems: OptionsSelect[] = [
+  { value: 'high', name: 'Сильно хочу' },
+  { value: 'medium', name: 'Средне хочу' },
+  { value: 'low', name: 'Могу подождать' },
+];
+
+interface IProps {
+  /** Title form. */
+  readonly title: string;
+
+  /** Form action */
+  readonly actionName: string;
+
+  /** Handles submit on element. */
+  // eslint-disable-next-line no-unused-vars
+  readonly callbackSubmitForm: (itemFormValue: ItemFormValue) => void;
+}
+
+export const WishItemForm: FC<IProps> = ({
+  callbackSubmitForm,
+  actionName,
+  title,
+}) => {
+  const handleSubmitForm = async (itemFormValue: ItemFormValue) => {
+    callbackSubmitForm(itemFormValue);
+    // eslint-disable-next-line no-use-before-define
+    formik.resetForm();
+  };
+
+  const formik = useFormik({
+    initialValues: initialFormValues,
+    validationSchema: WishItemFormSchema,
+    onSubmit: handleSubmitForm,
+  });
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <FormikProvider value={formik}>
+        <Form>
+          <FormikControl
+            control="input"
+            name="name"
+            type="text"
+            placeholder="Название"
+          />
+          <FormikControl
+            control="input"
+            name="description"
+            type="text"
+            placeholder="Описание"
+          />
+          <FormikControl
+            control="input"
+            name="price"
+            type="text"
+            placeholder="Цена"
+          />
+          <FormikControl
+            control="input"
+            name="imageRef"
+            type="text"
+            placeholder="Ссылка на картинку"
+          />
+          <FormikControl
+            control="select"
+            name="priority"
+            options={optionItems}
+          />
+          <Button style={buttonFormStyle} type="submit">
+            {actionName}
+          </Button>
+        </Form>
+      </FormikProvider>
+    </>
+  );
+};
---
+++ src/features/wishlist/components/WishItemForm/styles.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+import { generalColors } from '../../../../theme/variable';
+
+export const buttonFormStyle = css({
+  backgroundColor: generalColors.secondary,
+  color: generalColors.primary,
+  border: 'none',
+});
